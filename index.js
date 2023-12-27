fc9db2acf595f9ae569d033e7ff0961d6444978f
veseo <hernicetits@gmail.com>
2023-12-27 12:16:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.use('/', async (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
